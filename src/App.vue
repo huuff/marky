083d9f9a0ca8c3ff69b35358e0a025bb105b0667
Haf <haf@protonmail.ch>
2021-11-15 20:43:56 +0100

    added file selector (not working)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <main class="container mx-auto vh-100">
     <h1 class="display-2 text-center mb-3">Marky</h1>
-    <the-name class="mb-2"></the-name>
+    <div class="row mb-2">
+      <the-name class="col"></the-name>
+      <the-file-selector class="col" :files="files"></the-file-selector>
+    </div>
     <div class="row h-75">
       <the-editor class="col" v-model="text"></the-editor>
       <the-render class="col" :input="text"></the-render>
@@ -14,16 +17,20 @@ import { defineComponent } from 'vue';
 import TheEditor from './components/TheEditor.vue';
 import TheRender from './components/TheRender.vue';
 import TheName from './components/TheName.vue';
+import TheFileSelector from './components/TheFileSelector.vue';
+
 
 export default defineComponent({
   name: 'App',
   data: () => ({
-    text: ""
+    text: "",
+    files: [],
   } as { 
     text: string;
+    files: string[];
   }),
   components: {
-    TheEditor, TheRender, TheName,
+    TheEditor, TheRender, TheName, TheFileSelector
   },
 });
 </script>
---
+++ src/components/TheFileSelector.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex flex-row align-items-baseline">
+    <label class="text-muted text-nowrap me-3" for="fileSelector">Current file: </label> 
+    <select class="form-select flex-grow-1" id="fileSelector"> 
+      <option :selected="files.length === 0"> Choose one</option>
+      <option v-for="file in files" :key="`file-${file}`">{{ file }}</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    files: {
+      type: Array as PropType<Array<string>>,
+      required: true
+    },
+  },
+});
+</script>
