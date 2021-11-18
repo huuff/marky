694b357c2a74f912ace12b34b45d32d8f3c92f36
Haf <haf@protonmail.ch>
2021-11-18 15:51:07 +0100

    no more weird stuff with the selector

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 # Tasks
-* `Choose one` is not a valid route
 * Get enter key to set name, instead of only blur
--- src/components/TheFileSelector.vue
+++ src/components/TheFileSelector.vue
@@ -2,7 +2,7 @@
   <div class="d-flex flex-row align-items-baseline">
     <label class="text-muted text-nowrap me-3" for="fileSelector">Current file: </label> 
     <select class="form-select flex-grow-1" id="fileSelector" @change="setFile"> 
-      <option :selected="files.length === 0" :value="null"> Choose one</option>
+      <option :selected="files.length === 0" value=""> Choose one</option>
       <option v-for="file in files" :key="`file-${file}`" :value="file" :selected="file === current">{{ file }}</option>
     </select>
   </div>
