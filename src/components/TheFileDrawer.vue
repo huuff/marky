6bdd43714e4b0ba78db9e12f7eb7f740fa3a1de1
Haf <haf@protonmail.ch>
2021-11-29 16:52:04 +0100

    starting with filedrawer

---
+++ src/components/TheFileDrawer.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+  <div class="offcanvas-header">
+    <h5 id="offcanvasTopLabel">Saved files</h5>
+    <button type="button" class="btn-close text-reset" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div v-for="file in files">
+      {{ file }}
+    </div>
+  </div>
+</div> 
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  setup() {
+   const store = useStore(); 
+
+   const files = store.getters.fileNames;
+  }
+});
+</script>
+
+<style>
+</style>
